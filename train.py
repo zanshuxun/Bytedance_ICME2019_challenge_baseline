86045f5b2cc3ec98f5a9587939e4d8b57c3fe007
631763140 <631763140@qq.com>
2021-05-18 00:33:48 +0800

    update the code for DeepCTR 0.8.5

--- train.py
+++ train.py
@@ -1,30 +1,32 @@
 import pandas as pd
-from deepctr import SingleFeat
+from deepctr.feature_column import SparseFeat, DenseFeat, get_feature_names
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 from model import xDeepFM_MTL
 
-ONLINE_FLAG = True
+ONLINE_FLAG = False
 loss_weights = [1, 1, ]  # [0.7,0.3]任务权重可以调下试试
 VALIDATION_FRAC = 0.2  # 用做线下验证数据比例
 
 if __name__ == "__main__":
-    data = pd.read_csv('./input/final_track2_train.txt', sep='\t', names=[
-        'uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish', 'like', 'music_id', 'did', 'creat_time', 'video_duration'])
+    data = pd.read_csv('./input/final_track2_train_200.txt', sep='\t',
+                       names=['uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish', 'like',
+                              'music_id', 'did', 'creat_time', 'video_duration'])
     if ONLINE_FLAG:
-        test_data = pd.read_csv('./input/final_track2_test_no_anwser.txt', sep='\t', names=[
-                                'uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish', 'like', 'music_id', 'did', 'creat_time', 'video_duration'])
+        test_data = pd.read_csv('./input/final_track2_test_no_anwser.txt', sep='\t',
+                                names=['uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel', 'finish',
+                                       'like', 'music_id', 'did', 'creat_time', 'video_duration'])
         train_size = data.shape[0]
         data = data.append(test_data)
     else:
-        train_size = int(data.shape[0]*(1-VALIDATION_FRAC))
+        train_size = int(data.shape[0] * (1 - VALIDATION_FRAC))
 
     sparse_features = ['uid', 'user_city', 'item_id', 'author_id', 'item_city', 'channel',
                        'music_id', 'did', ]
     dense_features = ['video_duration']  # 'creat_time',
 
     data[sparse_features] = data[sparse_features].fillna('-1', )
-    data[dense_features] = data[dense_features].fillna(0,)
+    data[dense_features] = data[dense_features].fillna(0, )
 
     target = ['finish', 'like']
 
@@ -34,34 +36,34 @@ if __name__ == "__main__":
     mms = MinMaxScaler(feature_range=(0, 1))
     data[dense_features] = mms.fit_transform(data[dense_features])
 
-    sparse_feature_list = [SingleFeat(feat, data[feat].nunique())
-                           for feat in sparse_features]
-    dense_feature_list = [SingleFeat(feat, 0)
-                          for feat in dense_features]
+    fixlen_feature_columns = [SparseFeat(feat, data[feat].nunique())  for feat in sparse_features]+[DenseFeat(feat, 1) for feat in dense_features]
+
+    dnn_feature_columns = fixlen_feature_columns
+    linear_feature_columns = fixlen_feature_columns
+
+    feature_names = get_feature_names(linear_feature_columns + dnn_feature_columns)
 
     train = data.iloc[:train_size]
     test = data.iloc[train_size:]
-
-    train_model_input = [train[feat.name].values for feat in sparse_feature_list] + \
-        [train[feat.name].values for feat in dense_feature_list]
-    test_model_input = [test[feat.name].values for feat in sparse_feature_list] + \
-        [test[feat.name].values for feat in dense_feature_list]
+    train_model_input = {name: train[name] for name in feature_names}
+    test_model_input = {name: test[name] for name in feature_names}
 
     train_labels = [train[target[0]].values, train[target[1]].values]
     test_labels = [test[target[0]].values, test[target[1]].values]
 
-    model = xDeepFM_MTL({"sparse": sparse_feature_list,
-                         "dense": dense_feature_list})
-    model.compile("adagrad", "binary_crossentropy", loss_weights=loss_weights,)
+    model = xDeepFM_MTL(linear_feature_columns, dnn_feature_columns)
+    # model = xDeepFM_MTL({"sparse": sparse_feature_list,
+    #                      "dense": dense_feature_list})
+    model.compile("adagrad", "binary_crossentropy", loss_weights=loss_weights, )
 
     if ONLINE_FLAG:
         history = model.fit(train_model_input, train_labels,
                             batch_size=4096, epochs=1, verbose=1)
-        pred_ans = model.predict(test_model_input, batch_size=2**14)
+        pred_ans = model.predict(test_model_input, batch_size=2 ** 14)
 
     else:
         history = model.fit(train_model_input, train_labels,
-                            batch_size=4096, epochs=1, verbose=1, validation_data=(test_model_input, test_labels))
+                            batch_size=4096, epochs=5, verbose=1, validation_data=(test_model_input, test_labels))
 
     if ONLINE_FLAG:
         result = test_data[['uid', 'item_id', 'finish', 'like']].copy()
